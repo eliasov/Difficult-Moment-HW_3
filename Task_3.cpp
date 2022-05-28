3de71096a103a3f7dc8aa3541f32dce41e1b7ec4
Ilya Ausiannikau <ilya.ovsyannikov.1997@mail.ru>
2022-05-28 10:29:43 +0300

    HomeWork_3

---
+++ Task_3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <random>
+
+using namespace std;
+
+//====================================
+// Task_2
+//====================================
+
+template <class T>
+class Array
+{
+private:
+	T* arr = nullptr;
+	int size;
+public:
+	Array(int s = 1) : size(s)
+	{
+		arr = new T[s];
+	};
+
+	~Array() { delete[] arr; }
+
+	T& operator [] (const int& s)
+	{
+		if (s > 0 && s < size)
+		{
+			return arr[s];
+		}
+		return arr[0];
+	};
+
+
+	class MyIterator
+	{
+	private:
+		T* current;
+	public:
+		MyIterator(T* itr) : current(itr) {}
+		~MyIterator() = default;
+
+		T& operator +  (int val) { return *(current + val); };
+		T& operator -  (int val) { return *(current - val); };
+		T& operator ++ (int) { return *(++current); };
+		T& operator -- (int) { return *(--current); };
+		T& operator ++ () { return *(++current); };
+		T& operator -- () { return *(--current); };
+		T& operator *  () { return *(current); };
+
+		bool operator != (const MyIterator& it) { return current != it.current; };
+		bool operator == (const MyIterator& it) { return current == it.current; };
+
+	};
+
+MyIterator begin() { return (arr); };
+MyIterator end() { return (arr + size); };
+
+};
+
